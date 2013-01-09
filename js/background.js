ac6ee982d47105cd24fe198d0b5d7c555e0ec14e
北河 <itbeihe@gmail.com>
2013-01-09 23:26:31 +0800

    background coord

--- js/background.js
+++ js/background.js
@@ -5,11 +5,48 @@
 */
 
 (function($) {
+
+
+    var fontDiv = null;
+    
+    var getWidthHeight = function(keyObj){
+        if(!fontDiv){
+            fontDiv = document.createElement('div');
+            fontDiv.style.position = 'absolute';
+            fontDiv.style.top = '0';
+            document.body.appendChild(fontDiv);
+        }
+        fontDiv.innerHTML = keyObj.keyword;
+        fontDiv.style.fontSize = keyObj.fontSize;  
+        fontDiv.style.fontFamily = keyObj.fontFamily;
+        fontDiv.style.visibility = 'hidden';
+        fontDiv.style.zIndex = '-100';
+        return {
+            width:$(fontDiv).width(),
+            height:$(fontDiv).height()
+        }
+    };
+    
+    
+    var getCoord = function(keyObj,maxWidht,maxHeight){
+        var WH = getWidthHeight(keyObj);
+        var width = WH.width;
+        var height = WH.height;
+        var x = parseInt(Math.random()*(maxWidht-2*width)+width);
+        var y = parseInt(Math.random()*(maxHeight-2*height)+height);
+        return {
+            x:x,
+            y:y,
+            width:width,
+            height:WH.height
+        }
+    }
+    
     $.keywordbg = function(customConfig) {
         this.config = {
             canvasId : 'parallax-bg',
-            keywords : ['CSS3','HTML5','javascript','Node.js','Notepad++','JAVA','Linux','GitHub','PhotoShop'],
-            zindexLevel : 3,
+            keywords : ['CSS3','HTML5','javascript','Node.js','JAVA','YUI','Linux'],
+            zSpeedLevel : 3,
             minFontSize:40,
             maxFontSize:60,
             fontFamily:'impact'
@@ -23,14 +60,18 @@
         init:function(){
             var self = this;
             self.canvasObj = document.getElementById(self.config.canvasId);
-            self.canvasObj.width = $(document).width() < $('body').width() ? $(document).width() : $('body').width();
-            self.canvasObj.height = $(document).height() < $('body').height() ? $(document).height() : $('body').height();
-            console.log(self.canvasObj);
+            self.canvasObj.width = $(window).width() ;
+            self.canvasObj.height = $(window).height();
+            //console.log(self.canvasObj.width);
+            //console.log(self.canvasObj.height);
             //try{
                 self.context = self.canvasObj.getContext('2d');
-                console.log(self.context);
+                self.context.save();
+                //console.log(self.context);
                 self.keyObjs = self.buildKeyObjs();
-                self.fillKeyObjs();
+                window.setInterval(function(){
+                    self.fillKeyObjs();
+                },50)
                 
             //}catch(e){
             //    return;
@@ -43,55 +84,59 @@
             var keywords = self.config.keywords;
             var width = self.canvasObj.offsetWidth;
             var height = self.canvasObj.offsetHeight;
-            var zIndexLevel = self.config.zindexLevel;
+            var zSpeedLevel = self.config.zSpeedLevel;
             var keyObjs = [];
             if(keywords&&keywords[0]){
                 for(var i = 0,j=keywords.length;i<j;i++){
-                    if((typeof keywords[i]).toLowerCase() == 'string'){
-                        var fontSize = parseInt(Math.random()*(config.maxFontSize-config.minFontSize)+config.minFontSize);
-                        var x = parseInt(Math.random()*width);
-                        var y = parseInt(Math.random()*height);
-                        var index = parseInt(Math.random()*(zIndexLevel-1) + 1);
-                        var keyObj = {
-                            keyword : keywords[i],
-                            fontSize:50,
-                            position : [x,y],
-                            zIndex : index
-                        };
-                        keyObjs.push(keyObj);
-                    }else{
-                        var keyObj = $.extend({},keywords);
-                        if(!keyObj.position){
-                            var x = parseInt(Math.random()*width);
-                            var y = parseInt(Math.random()*height);
-                            keyObj.position = [x,y];
-                        }
-                        if(!keyObj.zIndex){
-                            var index = parseInt(Math.random()*(zIndexLevel-1) + 1);
-                            keyObj.zIndex = [x,y];
-                        }
-                        if(keyObj.keyword){
-                            keyObjs.push(keyObj);
-                        }
+                    var defObj = {
+                        fontSize:'50px',
+                        fontFamily:'impact',
+                        color:'#ccc'
                     }
+                    
+                    var keyObj = {};
+                     
+                    if((typeof keywords[i]).toLowerCase() == 'string'){
+                        keyObj = $.extend(defObj,{keyword : keywords[i]});
+                    }else{
+                        keyObj = $.extend(defObj,keywords);
+                    }
+
+                    if(!keyObj.zSpeed){
+                        var speed = parseInt(Math.random()*(zSpeedLevel-1) + 1);
+                        keyObj.zSpeed = speed;
+                    }
+                    keyObj.degree = Math.random()*36;
+                    
+                    keyObj.position = getCoord(keyObj,width,height);
+                    keyObjs.push(keyObj);
+                    
                 }
             }
             return keyObjs;
         },
         fillKeyObjs:function(){
+            //console.log('++');
             var self = this;
-            for(var i=0,len=self.keyObjs.length;i<1;i++){
+            var context = self.context;
+            context.clearRect(0,0,self.canvasObj.offsetWidth,self.canvasObj.offsetHeight);
+            for(var i=0,len=self.keyObjs.length;i<len;i++){
                 self.fillKeyObj(self.keyObjs[i]);
             }
+            
         },
         fillKeyObj:function(keyObj){
             var context = this.context;
-            context.font = '50px impact';
-            context.fillStyle = '#000';
-            console.log(keyObj.position[0]);
-            console.log(keyObj.position[1]);
-            context.fillText(keyObj.keyword,keyObj.position[0],keyObj.position[1]);
-            context.fillText('javascript',100,120);
+            context.font = '' + keyObj.fontSize + ' impact';
+            context.fillStyle = keyObj.color;
+            context.save();
+            context.translate(keyObj.position['x'],keyObj.position['y'])
+            //keyObj.position['y'] = keyObj.position['y'] + keyObj.zSpeed*5;
+            keyObj.degree = keyObj.degree+keyObj.zSpeed;
+            context.rotate(Math.PI/72*keyObj.degree);
+            //context.fillText(keyObj.keyword,keyObj.position['x'],keyObj.position['y']);
+            context.fillText(keyObj.keyword,-keyObj.position['width']/2,keyObj.position['height']/4);
+            //context.fillText('0,0',0,0);
             context.restore();
         }
     }
